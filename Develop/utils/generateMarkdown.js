4ef59f511d11205c9fa853fea99714a1f77f98ee
pra18apr <pra.18apr@gmail.com>
2023-06-21 19:25:33 -0600

    generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+const licenseBadge = (licenseConfirm, license, github, githubRepo) => {
+  if(licenseConfirm) {
+    const licenseLink = 'https://img.shields.io/github/license/' + github + '/' + githubRepo
+    return `![GitHub](${ licenseLink })`
+  } else {
+    license = encodeURI(license);
+    return `![license badge](https://img.shields.io/badge/license-${ license }-brightgreen)`
+  }
 }
+
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+  # ${ data.title }
+  
+  ${ licenseBadge(data.licenseConfirm, data.license, data.github, data.githubRepo) }
+  ## Description
+  ${ data.description }
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  ${ data.installation }
+  ## Usage
+  ${ data.usage }
+  ## License
+  This project uses ${ licenseBadge(data.licenseConfirm, data.license, data.github, data.githubRepo) }. You can get more information on licenses [here](https://choosealicense.com/).
+  ## Contributing
+  ${ data.contributing}
+  ## Tests
+  ${ data.tests }
+  ## Questions
+  * [My GitHub Profile](https://github.com/${ data.github })
+  * Email me at: [${ data.email }](mailto:${ data.email }) with questions about this project
+`;
+};
 
 module.exports = generateMarkdown;
